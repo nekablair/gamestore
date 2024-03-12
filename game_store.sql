98d233a373db46a11535e267f8bffc702533dbdf
Neka Blair <57186734+nekablair@users.noreply.github.com>
2024-03-12 09:46:52 -0600

    add select statements as a check

--- game_store.sql
+++ game_store.sql
@@ -1,8 +1,5 @@
 -- Drop the table if it exists
 DROP TABLE IF EXISTS games;
-DROP TABLE IF EXISTS action_figure;
-DROP TABLE IF EXISTS poster;
-DROP TABLE IF EXISTS employee;
 
 -- Create Games Table
 CREATE TABLE games (
@@ -14,6 +11,12 @@ CREATE TABLE games (
     price DECIMAL(5, 2) NOT NULL CHECK(price > 10 AND price < 60)
 );
 
+\COPY games FROM '/home/neka/projects/gamestore/data/game.csv' WITH CSV HEADER;
+SELECT * FROM games;
+
+
+DROP TABLE IF EXISTS action_figure;
+
 CREATE TABLE action_figure (
     figure_id INT PRIMARY KEY,
     action_figure_title VARCHAR(100) 
@@ -24,6 +27,11 @@ CREATE TABLE action_figure (
     price DECIMAL(4, 2)
         NOT NULL CHECK (price >= 10 AND price <= 100)
 );
+\COPY action_figure FROM '/home/neka/projects/gamestore/data/action_figure.csv' WITH CSV HEADER;
+SELECT * FROM action_figure 
+
+
+DROP TABLE IF EXISTS poster;
 
 CREATE TABLE poster (
     poster_id INT PRIMARY KEY,
@@ -33,6 +41,11 @@ CREATE TABLE poster (
     quantity INT CHECK (quantity > 0 AND quantity <= 30),
     price DECIMAL(4, 2) NOT NULL CHECK (price >= 6 AND price <= 20)
 );
+\COPY poster FROM '/home/neka/projects/gamestore/data/poster.csv' WITH CSV HEADER;
+SELECT * FROM poster;
+
+
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE employee (
     employee_id INT PRIMARY KEY,
@@ -46,10 +59,8 @@ CREATE TABLE employee (
 );
 --DID CALCULATIONS BY MULTIPLYING DOLLARS PER HR BY 40/HRS PER WEEK
 
-\COPY games FROM '/home/neka/projects/gamestore/data/game.csv' WITH CSV HEADER;
-\COPY action_figure FROM '/home/neka/projects/gamestore/data/action_figure.csv' WITH CSV HEADER;
-\COPY poster FROM '/home/neka/projects/gamestore/data/poster.csv' WITH CSV HEADER;
 \COPY employee FROM '/home/neka/projects/gamestore/data/employee.csv' WITH CSV HEADER;
+SELECT * FROM employee;
 
 -- in psql shell
 -- run the following command starting at the backslash and i in your DB
